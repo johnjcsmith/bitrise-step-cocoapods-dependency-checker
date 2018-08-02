9918b76dc097194b319920bb71c66463f47c12c6
John Smith <john@johnjcsmith.com>
2018-08-02 15:03:39 +0930

    Fixes YAML syntax issues

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-dependency-check --failOnCVSS=$fail_on_cvss_level --enableExperimental --project $BITRISE_APP_TITLE --out $BITRISE_DEPLOY_DIR --scan $podfile_lock_path
+dependency-check --failOnCVSS=$fail_on_cvss_level --enableExperimental --project $app_title --out $deploy_dir --scan $podfile_lock_path
